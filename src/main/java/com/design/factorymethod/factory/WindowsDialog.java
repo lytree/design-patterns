6c3b71496d237969edcbedb3b758d51d4a01aca5
Pride Yang <pride.yang@outlook.com>
2020-10-09 14:01:26 +0800

    工厂方法

--- src/main/java/com/design/factorymethod/factory/WindowsDialog.java
+++ src/main/java/com/design/factorymethod/factory/WindowsDialog.java
@@ -1,4 +1,11 @@
 package com.design.factorymethod.factory;
 
+import com.design.factorymethod.Button;
+import com.design.factorymethod.WindowsButton;
+
 public class WindowsDialog extends Dialog{
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
 }
