f1f39b498a75522d5be9e9613537c85a51e69f19
Pride Yang <pride.yang@outlook.com>
2020-10-09 11:25:44 +0800

    工厂方法

--- src/main/java/com/design/factorymethod/Demo.java
+++ src/main/java/com/design/factorymethod/Demo.java
@@ -1,4 +1,24 @@
 package com.design.factorymethod;
 
+import com.design.factorymethod.factory.Dialog;
+import com.design.factorymethod.factory.HtmlDialog;
+import com.design.factorymethod.factory.WindowsDialog;
+
 public class Demo {
+    private static Dialog dialog;
+
+    public static void main(String[] args) {
+        configure();
+        runBusinessLogic();
+    }
+    public static void configure() {
+        if (System.getProperty("os.name").equals("Windows 10")) {
+            dialog = new WindowsDialog();
+        }else {
+            dialog = new HtmlDialog();
+        }
+    }
+    public static void runBusinessLogic(){
+        dialog.renderWindow();
+    }
 }
