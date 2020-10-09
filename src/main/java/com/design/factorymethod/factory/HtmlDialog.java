5ca77d62d1b835daa1c2bff2400a40c3f44b8d4f
Pride Yang <pride.yang@outlook.com>
2020-10-09 14:00:49 +0800

    工厂方法

--- src/main/java/com/design/factorymethod/factory/HtmlDialog.java
+++ src/main/java/com/design/factorymethod/factory/HtmlDialog.java
@@ -1,4 +1,11 @@
 package com.design.factorymethod.factory;
 
+import com.design.factorymethod.Button;
+import com.design.factorymethod.HtmlButton;
+
 public class HtmlDialog extends Dialog{
+    @Override
+    public Button createButton() {
+        return new HtmlButton();
+    }
 }
